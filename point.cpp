187efc76195a44a83afa98cf1d9279a73c9d2fa1
Райхман Иван ТКИ-141 <113427606+ReichmanIvan@users.noreply.github.com>
2023-05-21 22:15:35 +0300

    Update point.cpp

--- point.cpp
+++ point.cpp
@@ -1,6 +1,7 @@
 #include "point.h"
+#include "help_mathr.h"
 
-miit::line_lib::point::point(double value_x, double value_y)
+miit::line_lib::point::point(const double value_x, const double value_y)
 	:x(value_x), y(value_y)
 {
 }
@@ -13,4 +14,9 @@ double miit::line_lib::point::get_x() const
 double miit::line_lib::point::get_y() const
 {
 	return this->y;
+}
+
+bool miit::line_lib::operator==(const point& lha, const point& rha)
+{
+	return (miit::help_math::is_double_equal(lha.x, rha.x) && miit::help_math::is_double_equal(lha.y, rha.y));
 }
