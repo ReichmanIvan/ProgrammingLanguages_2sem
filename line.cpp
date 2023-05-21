a0a96e7c7a61f6aa8f2c614009853cfa9a4ced97
Райхман Иван ТКИ-141 <113427606+ReichmanIvan@users.noreply.github.com>
2023-05-21 22:16:32 +0300

    Update line.cpp

--- line.cpp
+++ line.cpp
@@ -1,8 +1,12 @@
 #include "line.h"
 
-miit::line_lib::line::line(miit::line_lib::point point_1, miit::line_lib::point point_2, miit::line_lib::colour colour)
-	:point_1(point_1), point_2(point_2), colour(colour)
+miit::line_lib::line::line(const miit::line_lib::point& point_1, const miit::line_lib::point& point_2, const miit::line_lib::color& color)
+	:point_1(point_1), point_2(point_2), color(color)
 {
+	if (point_1 == point_2)
+	{
+		throw std::out_of_range("Not line");
+	}
 }
 
 miit::line_lib::point miit::line_lib::line::get_point_1() const
@@ -15,7 +19,7 @@ miit::line_lib::point miit::line_lib::line::get_point_2() const
 	return this->point_2;
 }
 
-miit::line_lib::colour miit::line_lib::line::get_colour() const
+miit::line_lib::color miit::line_lib::line::get_color() const
 {
-	return this->colour;
+	return this->color;
 }
