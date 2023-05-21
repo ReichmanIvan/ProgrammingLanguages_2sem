75f66b511fb222e3b0c7667c20799d43052d05f1
Райхман Иван ТКИ-141 <113427606+ReichmanIvan@users.noreply.github.com>
2023-05-21 22:16:50 +0300

    Update colour.h

--- colour.h
+++ colour.h
@@ -2,10 +2,10 @@
 #include <iostream>
 namespace miit::line_lib
 {
-    class colour
+    class color
     {
     public:
-        colour(int red = 0, int green = 0, int blue = 0);
+        color(const int red = 0, const int green = 0, const int blue = 0);
         int get_red() const;
         int get_green() const;
         int get_blue() const;
