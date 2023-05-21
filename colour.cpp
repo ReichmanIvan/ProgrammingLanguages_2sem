714e3bfabff1113894598bb0549fcfbf4cbf9b08
Райхман Иван ТКИ-141 <113427606+ReichmanIvan@users.noreply.github.com>
2023-05-21 22:17:09 +0300

    Update colour.cpp

--- colour.cpp
+++ colour.cpp
@@ -1,21 +1,25 @@
-#include "colour.h"
+#include "color.h"
 
-miit::line_lib::colour::colour(int red, int green, int blue)
+miit::line_lib::color::color(const int red, const int green, const int blue)
 	:red(red), green(green), blue(blue)
 {
+	if (red < 0 || red > 255 || green < 0 || green > 255 || blue < 0 || blue > 255)
+	{
+		throw std::out_of_range("Not correct color");
+	}
 }
 
-int miit::line_lib::colour::get_red() const
+int miit::line_lib::color::get_red() const
 {
 	return this->red;
 }
 
-int miit::line_lib::colour::get_green() const
+int miit::line_lib::color::get_green() const
 {
 	return this->green;
 }
 
-int miit::line_lib::colour::get_blue() const
+int miit::line_lib::color::get_blue() const
 {
 	return this->blue;
 }
