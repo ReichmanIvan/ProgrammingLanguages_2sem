41b38ba64a2daf964e2ca7d60678f608889a1426
Райхман Иван ТКИ-141 <113427606+ReichmanIvan@users.noreply.github.com>
2023-05-21 22:16:08 +0300

    Update line.h

--- line.h
+++ line.h
@@ -1,19 +1,19 @@
 #pragma once
 #include <iostream>
 #include "point.h"
-#include "colour.h"
+#include "color.h"
 namespace miit::line_lib
 {
     class line
     {
     public:
-        line(point point_1, point point_2, colour colour);
+        line(const point& point_1, const point& point_2, const color& color);
         point get_point_1() const;
         point get_point_2() const;
-        colour get_colour() const;
+        color get_color() const;
     private:
         point point_1;
         point point_2;
-        colour colour;
+        color color;
     };
 }
