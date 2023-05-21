31676ba92e606525d2453d6916255b8a1d0c6ea0
Райхман Иван ТКИ-141 <113427606+ReichmanIvan@users.noreply.github.com>
2023-05-21 22:15:53 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include "..\line_lib\point.h"
-#include "..\line_lib\colour.h"
+#include "..\line_lib\color.h"
 using namespace miit::line_lib;
 
 int main()
 {
 	auto point_1 = point(1.5, 2.5);
 	std::cout << point_1.get_x() << std::endl;
-	auto colour_1 = colour(123, 231, 213);
-	std::cout << colour_1.get_blue();
+	auto color_1 = color(123, 231, 213);
+	std::cout << color_1.get_blue();
 	return 0;
 }
