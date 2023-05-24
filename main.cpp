3a8bf643b0fe8295a7713425076458fba11bf9bc
Райхман Иван ТКИ-141 <113427606+ReichmanIvan@users.noreply.github.com>
2023-05-24 20:44:20 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,9 +4,10 @@ using namespace line_lib;
 
 int main()
 {
-	auto point_1 = point(1.5, 2.5);
-	std::cout << point_1.get_x() << std::endl;
-	line_col::color color_1 = line_col::color(123, 231, 213);
-	std::cout << color_1.get_blue();
+	point point_1(123.0, 21.0);
+	point point_2(57.0, 35.0);
+	line_col::color color_1(12, 247, 102);
+	line line_1(point_1, point_2, color_1);
+	std::cout << line_1;
 	return 0;
 }
