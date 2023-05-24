bcf29d164c3c7897682843d138387f6ff968d801
Райхман Иван ТКИ-141 <113427606+ReichmanIvan@users.noreply.github.com>
2023-05-24 20:48:55 +0300

    Update line.tests.cpp

--- line.tests.cpp
+++ line.tests.cpp
@@ -10,7 +10,7 @@ namespace linetests
 	{
 	public:
 		
-		TEST_METHOD(TestMethod1)
+		TEST_METHOD(point_overload_operator_equality)
 		{
 			//arrange
 			point first_point(5.6, 7.3);
@@ -21,6 +21,47 @@ namespace linetests
 
 			//assert
 			Assert::AreEqual(true, compare);
+		}
+
+		TEST_METHOD(point_construktor_is_not_null)
+		{
+			//arrange
+			point point(5,7);
+			std::string test_string;
+			std::stringstream buffer;
+
+			//act
+			buffer << point;
+			test_string = buffer.str();
+
+			//assert
+			Assert::IsNotNull(test_string.c_str());
+		}
+
+	
+		TEST_METHOD(line_wrong_color_exception)
+		{
+			Assert::ExpectException<std::out_of_range>([]()
+				{auto x = line_col::color(5, 7, 320);}
+			);
+		}
+
+		TEST_METHOD(line_construktor_is_not_null)
+		{
+			//arrange
+			point point1 = point(5, 7);
+			point point2 = point(6, 9);
+			line_col::color color1 = line_col::color(5, 7, 120);
+			line line(point1, point2, color1);
+			std::string test_string;
+			std::stringstream buffer;
+
+			//act
+			buffer << line;
+			test_string = buffer.str();
+
+			//assert
+			Assert::IsNotNull(test_string.c_str());
 		}
 	};
 }
