3b64ca7ceda6656b628f2a93a37f65be8200f9d6
Райхман Иван ТКИ-141 <113427606+ReichmanIvan@users.noreply.github.com>
2023-05-24 20:45:04 +0300

    Update line.cpp

--- line.cpp
+++ line.cpp
@@ -1,7 +1,8 @@
 #include "line.h"
 using namespace line_lib;
 
-line::line(const point point_1, const point point_2, const line_col::color color)
+line::line(const point point_1, const point point_2, const line_col::color color) :
+	point_1(point_1), point_2(point_2), color(color)
 {
 	if (point_1 == point_2)
 	{
@@ -22,4 +23,10 @@ point line::get_point_2() const
 line_col::color line::get_color() const
 {
 	return this->color;
+}
+
+std::ostream& operator<<(std::ostream& out, const line& line)
+{
+	out << "line(" << line.get_point_1() << ", " << line.get_point_2() << ", " << line.get_color() << ")";
+	return out;
 }
