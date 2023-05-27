b30073a0a25726d5f16c7556016fbe596dfde581
Райхман Иван ТКИ-141 <113427606+ReichmanIvan@users.noreply.github.com>
2023-05-27 15:59:08 +0300

    Update line.h

--- line.h
+++ line.h
@@ -4,17 +4,49 @@
 #include "color.h"
 using namespace line_lib;
 
-    class line
-    {
-    public:
-        line(const point point_1, const point point_2, const line_col::color color);
-        point get_point_1() const;
-        point get_point_2() const;
-        line_col::color get_color() const;
-        friend std::ostream& operator<< (std::ostream& out, const line& line);
-    private:
-        point point_1;
-        point point_2;
-        line_col::color color;
-    };
+/**
+ * \class line
+ * \brief Класс, представляющий линию, определенную двумя точками и цветом.
+ */
+class line
+{
+public:
+    /**
+     * \brief Конструктор класса line.
+     * \param point_1 Первая точка линии.
+     * \param point_2 Вторая точка линии.
+     * \param color Цвет линии.
+     */
+    line(const point point_1, const point point_2, const line_col::color color);
 
+    /**
+     * \brief Получает первую точку линии.
+     * \return Первая точка линии.
+     */
+    point get_point_1() const;
+
+    /**
+     * \brief Получает вторую точку линии.
+     * \return Вторая точка линии.
+     */
+    point get_point_2() const;
+
+    /**
+     * \brief Получает цвет линии.
+     * \return Цвет линии.
+     */
+    line_col::color get_color() const;
+
+    /**
+     * \brief Перегруженный оператор вывода в поток.
+     * \param out Поток вывода.
+     * \param line Линия для вывода.
+     * \return Поток вывода.
+     */
+    friend std::ostream& operator<< (std::ostream& out, const line& line);
+
+private:
+    point point_1; /**< Первая точка линии. */
+    point point_2; /**< Вторая точка линии. */
+    line_col::color color; /**< Цвет линии. */
+};
