b4ad2ef2429c3a0d1d418245c6fe1b26c679e516
Райхман Иван ТКИ-141 <113427606+ReichmanIvan@users.noreply.github.com>
2023-05-27 16:04:11 +0300

    Update help_math.cpp

--- help_math.cpp
+++ help_math.cpp
@@ -1,7 +1,18 @@
 #include "help_math.h"
 #include <cmath>
 
-bool miit::help_math::is_double_equal(const double value_1, const double value_2, const double precision)
+namespace miit::help_math
 {
-    return std::abs(value_1 - value_2) < precision;
+    /**
+     * \brief Сравнивает два значения типа double на равенство с учетом заданной точности.
+     * \param value_1 Первое значение типа double.
+     * \param value_2 Второе значение типа double.
+     * \param precision Точность сравнения.
+     * \return true, если значения value_1 и value_2 считаются равными с заданной точностью, иначе false.
+     */
+    bool is_double_equal(const double value_1, const double value_2, const double precision)
+    {
+        return std::abs(value_1 - value_2) < precision;
+    }
 }
+
